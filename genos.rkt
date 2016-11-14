98dbc55b5f4288706b652f5806dda5a593a76a09
speeeee <snsallay@gmail.com>
2016-11-13 22:02:13 -0500

    add parsing (untested)

--- genos.rkt
+++ genos.rkt
@@ -6,11 +6,15 @@
 ; *MUTABLE-VALUE*
 
 (struct Literal (val type) #:transparent)
-(struct Function (name in out) #:transparent)
+(struct Function (name ind in out runtime) #:transparent)
+; First, whatever is in runtime is run (could be id for non-runtime functions).
+
+(define (id x) x)
 
 ; *FLIST* :: [Function | SCOPE]
 ;   new functions are cons'ed in.
-(define *FLIST* (list 'scope))
+(define *FLIST* (list (Function "+" 1 '(int32 int32) 'int32 id) 'scope))
+(define *OUTPUT* '())
 
 (define b-app bytes-append)
 ; notation note: /= works for all types despite similar notation to `='.
@@ -20,12 +24,17 @@
 (define (uncurry a b) (lambda (x) (a b x)))
 (define uc uncurry)
 
+(define (flip a) (lambda (x y) (a y x)))
+(define (fork a f g) (lambda (x) (a (f x) (g x))))
+
 (define (splitf-at2 lst expr) (list (takef lst expr) (dropf lst expr)))
 
 (define (init- lst) (reverse (cdr (reverse lst))))
 
 (define (<< . a) (case (cdr a)
   [(()) a] [else (compose (car a) (<< . (cdr a)))]))
+; similar but not the same as Haskell's version of >>=
+(define (>>= a b) (if a (b a) 'nothing))
 
 (define (cons-lit a b) (if (string? (car b)) (cons `(,a) b) (cons (cons a (car b)) (cdr b))))
 
@@ -44,11 +53,19 @@
                          (Literal-val l)))))]
     [else #"Error"]))
 
+(define (f-find-and-apply sd . n)
+  (>>= (findf *FLIST* (lambda (x) (and (Function? x) (equal? (Function-name x) sd))))
+       (lambda (q) (f-apply q n))))
+
 (define (nfun! name in out) (set! *FLIST* (cons (Function name in out) *FLIST*)))
 (define (leave-scope! name in out) (set! *FLIST* (cdr (dropf *FLIST* (lambda (x) (/= x 'scope))))))
 
-; TODO: define `split' for expression of whitespace or ([{}]). splif-at recursive/fold.
+; DONE: define `split' for expression of whitespace or ([{}]). splif-at recursive/fold.
 ;   [] is unscoped expr, {} is scoped expr.
+
+(define (f-apply f lst) (Literal
+  (b-app (bytes-append* (map show lst)) (bytes 1) (integer->integer-bytes (Function-ind f)))
+  (Function-out f)))
 
 ; string->prog :: [String] -> (x :: [String | x])
 (define (ch-list->prog lst) (foldr (lambda (x n) (cond
@@ -64,7 +81,7 @@
   (if (member (car x) '((#\{) (#\[) (#\())) (ch-list->string x)
       (if (symbol? x) x (list->string x)))) lst))
 
-; tokenize :: (x :: [String | x]) -> (x :: [Literal | x])
+; tokenize :: (x :: [String | x]) -> (x :: [Literal])
 ;   reads each string (converted from char-list) and assigns some type.
 ;   special types include expr (#\[), scope-expr (#\{), and symbol, which is then checked as a
 ;     function in `parse'.
@@ -73,6 +90,12 @@
   [(string->number h) (let ([a (string->number h)]) (if (integer? h) (Literal h 'int32)
                                                                      (Literal h 'float64)))]
   [else (Literal h 'symbol)])) lst))
+
+(define (parse lst) (match lst
+  [(cons (Literal a 'expr) r) (parse (cons (parse a) r))]
+  [(list-rest a (Literal sd 'symbol) b) (f-find-and-apply sd a (parse b))]
+  [(cons (Literal m 'symbol) b) (f-find-and-apply m (parse b))]
+  [(cons a (list)) a]))
 
 (define test1 (ch-list->prog (string->list "ab bc cd")))
 (define test0 (Literal (list (Literal 1 'int32)) 'generator))
